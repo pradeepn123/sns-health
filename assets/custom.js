7b3da7b98f257e48bbc598872fca110ebb83ab15
hriks <hriks@outlook.com>
2023-05-22 18:27:30 +0530

    Rstructure video's and images

--- assets/custom.js
+++ assets/custom.js
@@ -1,91 +1,25 @@
-/**
- * Include your custom JavaScript here.
- *
- * We also offer some hooks so you can plug your own logic. For instance, if you want to be notified when the variant
- * changes on product page, you can attach a listener to the document:
- *
- * document.addEventListener('variant:changed', function(event) {
- *   var variant = event.detail.variant; // Gives you access to the whole variant details
- * });
- *
- * You can also add a listener whenever a product is added to the cart:
- *
- * document.addEventListener('product:added', function(event) {
- *   var variant = event.detail.variant; // Get the variant that was added
- *   var quantity = event.detail.quantity; // Get the quantity that was added
- * });
- *
- * If you are an app developer and requires the theme to re-render the mini-cart, you can trigger your own event. If
- * you are adding a product, you need to trigger the "product:added" event, and make sure that you pass the quantity
- * that was added so the theme can properly update the quantity:
- *
- * document.documentElement.dispatchEvent(new CustomEvent('product:added', {
- *   bubbles: true,
- *   detail: {
- *     quantity: 1
- *   }
- * }));
- *
- * If you just want to force refresh the mini-cart without adding a specific product, you can trigger the event
- * "cart:refresh" in a similar way (in that case, passing the quantity is not necessary):
- *
- * document.documentElement.dispatchEvent(new CustomEvent('cart:refresh', {
- *   bubbles: true
- * }));
- */
+document.addEventListener("DOMContentLoaded", function () {
+  var videoCover = document.getElementById("video--container");
+  if (videoCover) {
+    modal = videoCover.querySelector("#size-chart")
+    var span = videoCover.querySelector(".close")
+    const videoPlayerBtns = document.querySelectorAll("[data-video-button]")
+    videoElement = videoCover.querySelector("video")
 
-// image video slider on mobile screeen
-// mobileOnlySlider(".image-video-container", false, false, 641);
-// function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
-//   console.log('oooooooooooooo')
-//   var slider = $($slidername);
-//   var settings = {
-//     mobileFirst: true,
-//     autoplay: false,
-//     infinite: false,
-//     slidesToShow: 1.1,
-//     dots: $dots,
-//     arrows: $arrows,
-//     responsive: [
-//       {
-//         breakpoint: $breakpoint,
-//         settings: "unslick"
-//       }
-//     ]
-//   };
+    videoPlayerBtns.forEach((btn) => {
+      btn.addEventListener("click", function (e) {
+        videoElement.setAttribute("src", this.getAttribute("data-video-src"))
+        videoElement.load()
+        videoCover.style.display = "block";
+        const elemX = document.querySelector('html')
+        elemX.style.overflowY = 'hidden';
+      })
+    })
 
-//   slider.slick(settings);
-
-//   $(window).on("resize", function () {
-//     if ($(window).width() > $breakpoint) {
-//       return;
-//     }
-//     if (!slider.hasClass("slick-initialized")) {
-//       return slider.slick(settings);
-//     }
-//   });
-// }
-
-
-const html = document.html;
-var cover = document.getElementById("size-container");
-var modal = document.getElementById("size-chart");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-
-
-console.log('hlooooo')
-btn.onclick = function () {
-  console.log('---------------')
-  cover.style.display = "block";
-  const elemX = document.querySelector('html')
-  elemX.style.overflowY = 'hidden';
-}
-
-span.onclick = function () {
-  console.log('clooooooose')
-  cover.style.display = "none";
-  const elemX = document.querySelector('html')
-  elemX.style.overflowY = 'auto';
-}
-
+    span.addEventListener("click", function () {
+      videoCover.style.display = 'none';
+      const elemX = document.querySelector('html')
+      elemX.style.overflowY = 'auto';
+    })
+  }
+})
