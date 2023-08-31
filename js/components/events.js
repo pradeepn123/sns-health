db5aa0f89f05b1f32cb751563d32a2fa2294cc01
JingleBaba <umesh@shoptrade.co>
2023-08-31 19:37:12 +0530

    promo data changed to let

--- js/components/events.js
+++ js/components/events.js
@@ -18,7 +18,7 @@ export default GtmEvent;
 
 //utilities and events 
 export const getPromotionParams = () => {
-	const promotionData = {};
+	let promotionData = {};
 	const params = new Proxy(new URLSearchParams(window.location.search), {
 		get: (searchParams, prop) => searchParams.get(prop),
 });
