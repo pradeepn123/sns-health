a680babd72618342ba183ecba1ca2a9ddba85559
Brijesh Patil <brijesh@shoptrade.co>
2024-05-24 13:09:19 +0530

    count down banner section--updates

--- assets/countdown-banner.js
+++ assets/countdown-banner.js
@@ -34,9 +34,6 @@
         var date_string = month + '/' + day + '/' + year + ' ' + tarhour + ':' + tarmin;
         // Time without timezone
         this.countDownDate = new Date(year, month - 1, day, tarhour, tarmin, 0, 0).getTime();
-
-        // Time with timezone
-        this.automaticSaleTimer = this.dataset.automaticSaleTimer;
       }
       convertDateForIos(date) {
         var arr = date.split(/[- :]/);
@@ -52,73 +49,6 @@
         var _this = this;
         var updateTime = function updateTime() {
           var now = new Date();
-          if (_this.automaticSaleTimer) {
-            //core logic for timer:
-            //Get the starting day , ending day and the timezone
-            //Based on the timezone , convert the current date based on the timezone
-            //if starting day is current day, just update the hours in current date ,and convert it based on the timezone
-            //else , do the calculations based on the current date and get the future date 
-            //update the hours in future date , and convert it based on the timezone
-            //check if timer has started ,if yes, just set the distance or remaining time as the end date, show the sale end timer
-            //else, set the distance as the starting date, to show when sale will start
-
-            var dateData = _this.automaticSaleTimer.split(':'),
-              timeZoneOffset = "".concat(dateData[0]),
-              startDayIndex = parseInt(dateData[1]),
-              //indexed weekdays starting from sunday and 0 index, 0: sunday,
-              startHour = parseInt(dateData[2]),
-              startMinute = parseInt(dateData[3]),
-              endDayIndex = parseInt(dateData[4]),
-              //indexed weekdays starting from sunday and 0 index, 0: sunday,
-              endHour = parseInt(dateData[5]),
-              endMinute = parseInt(dateData[6]);
-            var getnextDay = (dateInstance, index) => {
-              if (!(dateInstance.getDay() == index)) {
-                dateInstance.setDate(dateInstance.getDate() + ((index + 7 - dateInstance.getDay()) % 7 || 7));
-              }
-              return dateInstance;
-            };
-            var _getConvertedTime = dateInstance => {
-              var localTime = dateInstance.getTime();
-              var convertedTime = moment__WEBPACK_IMPORTED_MODULE_0___default()(localTime).utcOffset(timeZoneOffset).format('YYYY-MM-DD HH:mm:ss');
-              return new Date(convertedTime);
-            };
-            var setHours = (dateInstance, hour, min) => {
-              return dateInstance.setHours(hour, min, 0, 0); //from shopify
-            };
-
-            //convert current time into given timezone into needed timezone
-            now = _getConvertedTime(now);
-            _this.countDownDate = _getConvertedTime(this.countDownDate);
-
-            //Get start day in milliseconds
-            var startDay = new Date();
-            startDay = _getConvertedTime(startDay);
-            startDay = getnextDay(startDay, startDayIndex);
-            startDay = setHours(startDay, startHour, startMinute);
-
-            //Get end day in milliseconds with timezone difference added
-            //Get start day in milliseconds
-            var endDay = new Date();
-            endDay = _getConvertedTime(endDay);
-            endDay = getnextDay(endDay, endDayIndex);
-            endDay = setHours(endDay, endHour, endMinute);
-            console.log(new Date(startDay), "Start day");
-            console.log(new Date(endDay), "End day");
-            console.log(new Date(now), "now");
-            var saleText = 'Starts';
-            _this.countDownDate = startDay;
-            var startingDistance = startDay - now; //check if sale has already started
-            //it will return 0 if sale has ended, in such case, start counting from the date of next sale
-
-            if (startingDistance < 0) {
-              //sale has ended so , look for next sale starting date
-              _this.countDownDate = endDay;
-              saleText = 'ENDS';
-              document.querySelector('.countdown-banner--timer .button').classList.add('visible');
-            }
-            document.querySelector('[data-timer-heading]').innerHTML = "SALE ".concat(saleText, " IN");
-          }
           now = getConvertedTime(now);
           var distance = _this.countDownDate - now;
 
--- js/sections/countdown-banner.js
+++ js/sections/countdown-banner.js
@@ -24,9 +24,6 @@ export default () => {
         let date_string = month + '/' + day + '/' + year + ' ' + tarhour + ':' + tarmin;
         // Time without timezone
         this.countDownDate = new Date(year, month - 1, day, tarhour, tarmin, 0, 0).getTime();
-
-        // Time with timezone
-        this.automaticSaleTimer = this.dataset.automaticSaleTimer;
       }
 
       convertDateForIos(date) {
@@ -43,82 +40,6 @@ export default () => {
         let _this = this;
         const updateTime = function () {
           let now = new Date();
-          if (_this.automaticSaleTimer) {
-            //core logic for timer:
-            //Get the starting day , ending day and the timezone
-            //Based on the timezone , convert the current date based on the timezone
-            //if starting day is current day, just update the hours in current date ,and convert it based on the timezone
-            //else , do the calculations based on the current date and get the future date 
-            //update the hours in future date , and convert it based on the timezone
-            //check if timer has started ,if yes, just set the distance or remaining time as the end date, show the sale end timer
-            //else, set the distance as the starting date, to show when sale will start
-
-            const dateData = _this.automaticSaleTimer.split(':'),
-            timeZoneOffset = `${dateData[0]}`,
-            startDayIndex = parseInt(dateData[1]), //indexed weekdays starting from sunday and 0 index, 0: sunday,
-            startHour = parseInt(dateData[2]),
-            startMinute = parseInt(dateData[3]),
-            endDayIndex = parseInt(dateData[4]), //indexed weekdays starting from sunday and 0 index, 0: sunday,
-            endHour = parseInt(dateData[5]),
-            endMinute = parseInt(dateData[6]);
-
-            const getnextDay = (dateInstance, index) => {
-              if (!(dateInstance.getDay() == index)) {
-                  dateInstance.setDate(dateInstance.getDate() + (((index + 7 - dateInstance.getDay()) % 7) || 7));
-              }
-              return dateInstance;
-            }
-
-            const getConvertedTime = (dateInstance) => {
-              const localTime = dateInstance.getTime();
-              const convertedTime = moment(localTime).utcOffset(timeZoneOffset).format('YYYY-MM-DD HH:mm:ss')
-              return new Date(convertedTime);
-            }
-
-            const setHours = (dateInstance, hour, min) => {
-              return dateInstance.setHours(hour, min, 0, 0) //from shopify
-            }
-
-
-            //convert current time into given timezone into needed timezone
-            now = getConvertedTime(now);
-            _this.countDownDate = getConvertedTime(this.countDownDate);
-
-            //Get start day in milliseconds
-            let startDay = new Date();
-            startDay = getConvertedTime(startDay);
-            startDay = getnextDay(startDay, startDayIndex);
-            startDay = setHours(startDay, startHour, startMinute)
-
-
-          //Get end day in milliseconds with timezone difference added
-        //Get start day in milliseconds
-              let endDay = new Date();
-              endDay = getConvertedTime(endDay);
-              endDay = getnextDay(endDay, endDayIndex);
-              endDay = setHours(endDay, endHour, endMinute)
-
-              console.log(new Date(startDay), "Start day");
-              console.log(new Date(endDay), "End day");
-              console.log(new Date(now), "now");
-
-
-            let saleText = 'Starts';
-            _this.countDownDate = startDay;
-            let startingDistance = startDay - now; //check if sale has already started
-            //it will return 0 if sale has ended, in such case, start counting from the date of next sale
-
-            if (startingDistance < 0) {
-              //sale has ended so , look for next sale starting date
-              _this.countDownDate = endDay;
-              saleText = 'ENDS';
-              document.querySelector('.countdown-banner--timer .button').classList.add('visible');
-            }
-
-      
-            document.querySelector('[data-timer-heading]').innerHTML = `SALE ${saleText} IN`;
-    
-          }
           now = getConvertedTime(now);
           const distance = _this.countDownDate - now;
 
